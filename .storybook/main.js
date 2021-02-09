c5c337b33f15fc7bf358766f91cf68d59761b5b3
rismanss <risman.job@gmail.com>
2021-02-09 16:32:01 +0700

    add addon for storybook

--- .storybook/main.js
+++ .storybook/main.js
@@ -1,5 +1,3 @@
-// .storybook/main.js
-
 module.exports = {
   stories: ['../template/**/*.stories.@(js|mdx)'],
   addons: ['@storybook/addon-essentials']
--- template/button/button.stories.js
+++ template/button/button.stories.js
@@ -1,6 +1,10 @@
 import React from 'react';
-import { storiesOf } from '@storybook/react';
-// import { action } from '@storybook/addon-actions';
+import { action } from '@storybook/addon-actions';
 import Button from './index';
 
-storiesOf('button').add('example', () => <Button />);
+export default {
+  title: 'Button',
+  component: Button,
+};
+
+export const Example = (args) => <Button {...args} onClick={action('onClick')} />;
